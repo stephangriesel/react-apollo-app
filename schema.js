7e5dbcc1cd2bbd252d6215d929fca0cf0fdbf4a0
Stephan Griesel <sgriesel@gmail.com>
2018-11-15 06:29:38 +0100

    update schema

---
+++ schema.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema } = require('graphql');
+
+// launch type
+const LaunchType = new GraphQLObjectType({
+    name:'Launch',
+    fields: () => ({
+        flight_number: {type: GraphQLInt },
+        mission_name: {type: GraphQLString },
+        launch_year: {type: GraphQLString },
+        launch_date_local: {type: GraphQLString },
+        launch_success: {type: GraphQLBoolean },
+        rocket: {type: RocketType },
+    })
+});
+
+// rocket type
+
+const RocketType = new GraphQLObjectType({
+    name:'Rocket',
+    fields: () => ({
+        rocket_id: {type: GraphQLString },
+        rocket_name: {type: GraphQLString },
+        rocket_type: {type: GraphQLString },
+    })
+});
+
+// root query
+const RootQuery = new GraphQLObjectType({
+    name:'Root Query Type',
+    fields: {
+        launches: {
+            type: new GraphQLList(LaunchType),
+            resolve(parent, args) {
+                return axios.get('https://api.spacexdata.com/v3/launches')
+                .then(res => res.data);
+
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
