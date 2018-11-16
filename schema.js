bba99fa33b3d0ce76208cf6cae1ead87871844af
Stephan Griesel <sgriesel@gmail.com>
2018-11-16 06:03:49 +0100

    add rockets to schema

--- schema.js
+++ schema.js
@@ -44,6 +44,34 @@ const RootQuery = new GraphQLObjectType({
                 .then(res => res.data);
 
             }
+        },
+        launch: {
+            type: LaunchType,
+            args: {
+                flight_number: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+                .then(res => res.data);
+            }
+        },
+        rockets: {
+            type: new GraphQLList(RocketType),
+            resolve(parent, args) {
+                return axios.get('https://api.spacexdata.com/v3/rockets')
+                .then(res => res.data);
+
+            }
+        },
+        rocket: {
+            type: LaunchType,
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                return axios.get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+                .then(res => res.data);
+            }
         }
     }
 });
